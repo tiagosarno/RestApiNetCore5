360394dfef15e2f26516a4f82ad199763d36a415
Tiago Rocha Sarno <tiago.sarno@compartilhaclub.com.br>
2021-01-21 23:01:12 -0300

    App .NET5 Teste

---
+++ RestApiNetCore5/RestApiNetCore5/Controllers/PersonController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiNetCore5.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PersonController : ControllerBase
+    {
+        private readonly ILogger<PersonController> _logger;
+
+        [HttpGet("sum/{firstNumber}/{secondNumber}")]
+        public IActionResult Sum(string firstNumber, string secondNumber)
+        {            
+            return BadRequest("Invalid Input");
+        }
+
+    }
+}
---
+++ RestApiNetCore5/RestApiNetCore5/Model/Person.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestApiNetCore5.Model
+{
+    public class Person
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Adress { get; set; }
+
+        public string Gender { get; set; }
+    }
+}

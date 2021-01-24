51201644cbf1edfc8494709d1f0dd246291c28e9
Tiago Rocha Sarno <tiago.sarno@compartilhaclub.com.br>
2021-01-24 13:04:14 -0300

    APIs funcionando Insomnia

--- RestApiNetCore5/RestApiNetCore5/Controllers/PersonController.cs
+++ RestApiNetCore5/RestApiNetCore5/Controllers/PersonController.cs
@@ -10,7 +10,7 @@ using System.Threading.Tasks;
 namespace RestApiNetCore5.Controllers
 {
     [ApiController]
-    [Route("[controller]")]
+    [Route("api/[controller]")]
     public class PersonController : ControllerBase
     {
         private readonly ILogger<PersonController> _logger;
